d08330c8ec1aeac496cdda9e39f3084f7c08bfe8
Margaux <mbavlsik@gmail.com>
2019-08-26 19:40:17 -0500

    mammal heartbeat stats

---
+++ calc_beat_lifetime.py
@@ -0,0 +1,10 @@
+import pandas as pd
+
+heart_info_df = pd.read_csv("file.csv")
+
+print(heart_info_df)
+#print(heart_info_df['Creature'])
+#print(heart_info_df['Mass (grams)'])
+#heart_info_df['Resting Heart Rate (BPM)']
+#heart_info_df['Longevity (Years)']
+
